cd719b9da8dfbb28616c79f8ab36f8d2fda498c7
DV <inge.diego94@gmail.com>
2025-08-05 13:45:51 -0500

    API

--- Api/index.js
+++ Api/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
-
+const clientes = require('./models/clientes')
 const app = express();
 app.use(express.json());
 
@@ -9,6 +9,53 @@ app.use(express.json());
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('Conectado a Mongo'))
 .catch(err => console.error('Error en la conexion', err));
+
+// Endpoint encargado de insertar un nuevo cliente.
+app.post('/clientes', async(req, res) => {
+    try {
+        const nuevoCliente = new clientes(req.body);
+        const clienteGuardado = nuevoCliente.save();
+        res.status(201).json(clienteGuardado);
+    } catch (error){
+        res.status(400).json({err: "Error al guardar el cliente"});
+    }
+    
+});
+
+// Metodo get
+app.get('/clientes', async (req, res) => {
+    try{
+        const obtenerClientes = await clientes.find();
+        res.json(obtenerClientes);
+    } catch(error){
+        res.status(500).json({error: 'Error al recuperar'});
+    }
+});
+
+
+//Metodo put por ID
+app.put('/clientes/:id', async(req, res) => {
+    try{
+        const clienteActualizado = await clientes.findByIdAndUpdate(
+            req.params.id, {
+                nombre: req.body.nombre,
+                edad: req.body.edad, 
+                direccion: req.body.direccion
+            },
+            { new: true}
+        );
+
+        if(!clienteActualizado) {
+            return res.status(404).json({error: 'Cliente no encontrado'});
+        }
+
+        res.json(clienteActualizado);
+
+    } catch(error){
+        res.status(400).json({error: 'Error al actualizar el cliente.', detalles:err})
+    }
+})
+
 
 
 // port
